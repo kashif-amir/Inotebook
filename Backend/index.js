8fd2188bc924a563d61df45bcc74d68404a915d4
kashif-amir <kashifamir174@gmail.com>
2025-12-21 00:36:36 +0500

    call the api

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,15 @@
 const connectToMongo = require('./db');
 connectToMongo();
-const express = require('express')
 
+const express = require('express')
 const app = express()
+
+var cors = require('cors')
 const port = 3000
+
 app.use(express.json());
+app.use(cors())
+
 
 //Avaliable routes
 app.use('/api/auth', require('./routes/auth'));
